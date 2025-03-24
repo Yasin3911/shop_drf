023da537f12f38cd7fbefa35ebd18b47ecb412a7
Yasin <yasinrfshe@gmail.com>
2025-03-24 21:19:16 +0330

    :sparkles: first view apis for brand, category and product

--- product/views.py
+++ product/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import Brand, Category, Product
+
+from .serializer import BrandSerializer, CategorySerializer, ProductSerializer
+
+
+class BrandViewSet(viewsets.ModelViewSet):
+    queryset = Brand.objects.all()
+    serializer_class = BrandSerializer
+
+    def list(self, request):
+        serializer_data = BrandSerializer(self.queryset, many=True)
+        return Response(serializer_data.data)
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+    def list(self, request):
+        serializer_data = CategorySerializer(self.queryset, many=True)
+        return Response(serializer_data.data)
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    def list(self, request):
+        serializer_data = ProductSerializer(self.queryset, many=True)
+        return Response(serializer_data.data)
