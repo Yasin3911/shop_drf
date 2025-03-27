c5804c4fd990c86f2f15c47aa472bfe2e993e221
Yasin <yasinrfshe@gmail.com>
2025-03-27 16:51:46 +0330

    :sparkles: Updated admin

--- product/admin.py
+++ product/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
 
-from .models import Brand, Product, Category
+from .models import Brand, Product, Category, ProductLine
+
+
+class ProductLineInline(admin.TabularInline):
+    model = ProductLine
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [ProductLineInline]
 
 admin.site.register(Brand)
 admin.site.register(Category)
-admin.site.register(Product)
+admin.site.register(ProductLine)
