d4ab9e0a79bee6e5584be1d27517f14ade4fb28f
Yasin <yasinrfshe@gmail.com>
2025-03-29 16:27:30 +0330

    :sparkles: added create, update and delete apis

--- product/serializer.py
+++ product/serializer.py
@@ -40,3 +40,60 @@ class ProductSerializer(serializers.ModelSerializer):
             'is_digital',
         ]
 
+    def create(self, validated_data):
+        brand_data = validated_data.pop('brand')
+        category_data = validated_data.pop('category')
+        product_lines_data = validated_data.pop('product_line')
+
+        brand, created = Brand.objects.get_or_create(name=brand_data['name'])
+        category, created = Category.objects.get_or_create(name=category_data['name'])
+
+        product = Product.objects.create(brand=brand, category=category, **validated_data)
+
+        for pld in product_lines_data:
+            ProductLine.objects.create(product=product, **pld)
+
+        return product
+
+    def update(self, instance, validated_data):
+        brand_data = validated_data.get('brand', None)
+        if brand_data:
+            brand, created = Brand.objects.get_or_create(name=brand_data['name'])
+            instance.brand = brand
+
+        category_data = validated_data.get('category', None)
+        if category_data:
+            category, created = Category.objects.get_or_create(name=category_data['name'])
+            instance.category = category
+
+        instance.name = validated_data.get('name', instance.name)
+        instance.description = validated_data.get('description', instance.description)
+        instance.price = validated_data.get('price', instance.price)
+        instance.is_digital = validated_data.get('is_digital', instance.is_digital)
+
+        product_lines_data = validated_data.get('product_line', None)
+        if product_lines_data:
+            self.update_product_lines(instance, product_lines_data)
+            # instance.product_line.all().delete()
+            # for pld in product_lines_data:
+            #     ProductLine.objects.create(product=instance, **pld)
+
+        instance.save()
+        return instance
+
+    def update_product_lines(self, product_instance, product_lines_data):
+        existing_product_lines = {obj.sku: obj for obj in product_instance.product_line.all()}
+
+        for product_line in product_lines_data:
+            sku = product_line['sku']
+            if sku in existing_product_lines:
+                line = existing_product_lines[sku]
+                for attr, value in product_line.items():
+                    setattr(line, attr, value)
+                line.save()
+                existing_product_lines.pop(sku)
+            else:
+                product_instance.product_line.create(**product_line)
+
+        for product_line in existing_product_lines.values():
+            product_line.delete()
--- product/views.py
+++ product/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from drf_spectacular.utils import extend_schema
 from rest_framework import viewsets, status
 from rest_framework.response import Response
 from rest_framework.decorators import action
@@ -30,23 +31,67 @@ class ProductViewSet(viewsets.ViewSet):
     def get_queryset(self):
         return Product.objects.isActive()
 
-
+    @extend_schema(responses=ProductSerializer)
     def list(self, request):
         serializer_data = ProductSerializer(self.get_queryset(), many=True)
-        return Response(serializer_data.data)
-
-    @action(methods=['get'], detail=False, url_path="category/(?P<category>.+)")
-    def get_list_with_category_filter(self, request, category=None):
-        serializer_data = ProductSerializer(self.get_queryset().filter(category__name=category), many=True)
         return Response(serializer_data.data)
 
     def retrieve(self, request, pk=None):
         # serializer_data = ProductSerializer(self.queryset.get(pk=pk))
         if pk.isdigit() == False:
-            return Response({'detail': 'Product ID is not valid'}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({'message': 'Product ID is not valid'}, status=status.HTTP_400_BAD_REQUEST)
 
         serializer_data = ProductSerializer(get_object_or_404(self.get_queryset(), pk=pk))
 
+        return Response(serializer_data.data)
+
+    @extend_schema(responses=ProductSerializer, request=ProductSerializer)
+    def create(self, request):
+        serializer_data = ProductSerializer(data=request.data)
+
+        if serializer_data.is_valid():
+            serializer_data.save()
+            return Response(serializer_data.data, status=status.HTTP_201_CREATED)
+        return Response(serializer_data.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    @extend_schema(responses=ProductSerializer, request=ProductSerializer)
+    def update(self, request, pk=None):
+        if pk.isdigit() == False:
+            return Response({'message': 'Product ID is not valid'}, status=status.HTTP_400_BAD_REQUEST)
+
+        product = get_object_or_404(self.get_queryset(), pk=pk)
+        serializer_data = ProductSerializer(product, data=request.data)
+
+        if serializer_data.is_valid():
+            serializer_data.save()
+            return Response(serializer_data.data, status=status.HTTP_200_OK)
+        return Response(serializer_data.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    @extend_schema(responses=ProductSerializer, request=ProductSerializer)
+    def partial_update(self, request, pk=None):
+        if pk.isdigit() == False:
+            return Response({'message': 'Product ID is not valid'}, status=status.HTTP_400_BAD_REQUEST)
+
+        product = get_object_or_404(self.get_queryset(), pk=pk)
+        serializer_data = ProductSerializer(product, data=request.data, partial=True)
+
+        if serializer_data.is_valid():
+            serializer_data.save()
+            return Response(serializer_data.data, status=status.HTTP_200_OK)
+        return Response(serializer_data.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    @extend_schema(responses=None)
+    def destroy(self, request, pk=None):
+        if pk.isdigit() == False:
+            return Response({'message': 'Product ID is not valid'}, status=status.HTTP_400_BAD_REQUEST)
+
+        product = get_object_or_404(self.get_queryset(), pk=pk)
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    @action(methods=['get'], detail=False, url_path="category/(?P<category>.+)")
+    def get_list_with_category_filter(self, request, category=None):
+        serializer_data = ProductSerializer(self.get_queryset().filter(category__name=category), many=True)
         return Response(serializer_data.data)
 
     @action(methods=['get'], detail=False, url_path="brand/(?P<brand>.+)")
